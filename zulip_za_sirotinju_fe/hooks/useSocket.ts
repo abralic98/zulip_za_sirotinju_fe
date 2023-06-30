f6a4ca8cd78db20124d035c3b03b34133a6be5b8
ante <twogamers1950@gmail.com>
2023-06-30 16:13:57 +0200

    fixes

--- zulip_za_sirotinju_fe/hooks/useSocket.ts
+++ zulip_za_sirotinju_fe/hooks/useSocket.ts
@@ -11,7 +11,7 @@ export const useSocket = () => {
       const absintheSocketInit = withAbsintheSocket.create(
         new PhoenixSocket(
           // process.env.WS_LINK || "ws://116.203.201.51:4000/api/graphql/socket",
-          process.env.WS_LINK || "ws://localhost:4000/api/graphql/socket",
+          process.env.WS_LINK || "ws://116.203.201.51:4000/api/graphql/socket",
           {
             params: {
               Authorization: `Bearer ${session?.user.token}`,
