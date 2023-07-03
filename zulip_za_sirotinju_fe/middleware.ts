577b49c4981468df4a85121c0cc2a1e7b23d8fb6
ante <twogamers1950@gmail.com>
2023-07-03 15:02:32 +0200

    middleware

--- zulip_za_sirotinju_fe/middleware.ts
+++ zulip_za_sirotinju_fe/middleware.ts
@@ -1,3 +1,43 @@
-export { default } from "next-auth/middleware"
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = { matcher: ["/app/:path*"] }
+export { default } from "next-auth/middleware";
+
+export const config = { matcher: ["/app/:path*"] };
+
+export async function middleware(request: NextRequest) {
+  const endpoint = process.env.NEXT_PUBLIC_GRAPHQL || "";
+
+  const redirect = () =>
+    NextResponse.redirect(new URL(process.env.NEXTAUTH_URL || "", request.url));
+
+  const token = request.cookies.get("next-auth.session-token")?.value;
+
+  const requestBody = {
+    query: `query {
+      me {
+         email
+      }
+    }`,
+  };
+
+  const headers = {
+    "content-type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify(requestBody),
+  };
+
+  try {
+    const res = await (await fetch(endpoint, options)).json();
+    if (!res.data.me) return redirect();
+  } catch (error) {
+    return redirect();
+  }
+
+  const response = NextResponse.next();
+  return response;
+}
